d6f062f70c0746967459270c073e39dee71d4a9d
Maciej Pokorski <maciekpokorski@gmail.com>
2022-03-31 18:40:54 +0200

    Load data to database

--- hello-world.py
+++ hello-world.py
@@ -2,8 +2,9 @@ import luigi
 import zipfile
 import boto3
 import botocore
-
+from sqlalchemy import create_engine
 from luigi.contrib.s3 import S3Client
+import pandas
 
 class S3Download(luigi.ExternalTask):
     
@@ -26,3 +27,26 @@ class UnZip(luigi.Task):
     def run(self):
         with zipfile.ZipFile(self.input().path) as zip_ref:
             zip_ref.extractall("./")
+
+class LoadCSVToDB(luigi.Task):
+    username = 'root'
+    password = 'root'
+    host = 'localhost'
+    port = 3306
+    DB_NAME = 'employees'
+
+    engine = create_engine(f"mysql://{username}:{password}@{host}:{port}/{DB_NAME}")
+
+    def requires(self):
+        return UnZip()
+
+    def run(self):
+        colnames=['emp_no', 'birth_date', 'first_name', 'last_name', 'gender', 'hire_date'] 
+        data = pandas.read_csv("employees.csv", names=colnames, parse_dates=['birth_date', 'hire_date'])
+        data.to_sql('employees', self.engine, index=False, if_exists='append')
+        
+    def exists(self):
+        query = "SELECT * FROM employees WHERE emp_no >= 500000"
+        with engine.connect() as conn:
+            results = conn.execute(query).fetchall()
+            return len(results) == 3
