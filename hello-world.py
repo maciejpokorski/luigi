405938524093c8c5b7d3c2e7811204694aa0fea9
Maciej Pokorski <maciekpokorski@gmail.com>
2022-03-31 17:15:14 +0200

    unzip downloaded s3 file

--- hello-world.py
+++ hello-world.py
@@ -15,3 +15,14 @@ class S3Download(luigi.ExternalTask):
     def run(self):
         return S3Client().get("s3://elasticbeanstalk-us-east-2-269645966560/employees.zip", self.zip_path)
 
+class UnZip(luigi.Task):
+
+    def requires(self):
+        return S3Download()
+
+    def output(self):
+        return luigi.LocalTarget("employees.csv")
+
+    def run(self):
+        with zipfile.ZipFile(self.input().path) as zip_ref:
+            zip_ref.extractall("./")
